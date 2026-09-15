c4eb3f717b2f29efcde7eb21231880173c366296
Dawson Friesenhahn <dawsonfriesenhahn@gmail.com>
2026-09-15 01:11:31 -0600

    Hey, the plane is at least on the globe now!

--- app/app.py
+++ app/app.py
@@ -1,11 +1,12 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, url_for
+import json
 
 app = Flask(__name__, )
 
 
 @app.route('/')
 def index():
-    return render_template("mapView.html")
+    return render_template("mapView.html", flight_info_route=url_for("fakeFlight"))
 
 @app.route('/services')
 def services():
@@ -14,6 +15,12 @@ def services():
 @app.route("/about")
 def about():
     return render_template("about.html")
+
+@app.route("/fakeFlight.json")
+def fakeFlight():
+
+    with open("flight.json", "r") as f:
+        return jsonify(json.load(f))
 
 
 if __name__ == "__main__":
